87db3966eb0848126f2b15e12947254c626b108a
Brath <brathbarbozaagustin@gmail.com>
2023-05-26 02:27:45 -0500

    modulo 1

---
+++ modulo-1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Escritura de la primera línea de código
+Console.WriteLine("Hello, World!");
+
+//Mostrar un nuevo mensaje
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+//Desafío: Escriba código en el editor de .NET para mostrar dos mensajes
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
